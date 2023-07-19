06017a46bf68aa4e7d3dbacb1c21f214026bbab3
ERA-YES <129915928+ERA-YES@users.noreply.github.com>
2023-07-19 10:08:19 +0800

    Add files via upload

--- vlfii.py
+++ vlfii.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import xml.dom.minidom as minidom
+import math
 
 root = ET.Element("FiiConfig", {"xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
                                 "xmlns:xsd": "http://www.w3.org/2001/XMLSchema"})
@@ -25,25 +26,18 @@ Format.text = "mp3"
 
 Flights = ET.SubElement(root, "Flights")
 
-Drone = [
-    [160, 160],
-    [280, 0],
-    [0, 0],
-    [100, 0],
-    [200, 0],
-]
-
-def start():
-    global FATHER, XML, WEBXML
+def start(d):
+    global FATHER, XML, WEBXML, DRONE
+    DRONE = d
     i = DRONE_NUM
     FlightView = ET.SubElement(Flights, "FlightView")
     Ip = ET.SubElement(FlightView, "Ip")
     Ip.text = "192.168.31.10{}".format(i+1)
     InitPos = ET.SubElement(FlightView, "InitPos")
     X = ET.SubElement(InitPos, "X")
-    X.text = str(Drone[i][0])
+    X.text = str(DRONE[i][0])
     Y = ET.SubElement(InitPos, "Y")
-    Y.text = str(Drone[i][1])
+    Y.text = str(DRONE[i][1])
     Code = ET.SubElement(FlightView, "Code")
     WEBXML = ET.SubElement(FlightView, "WebXml")
     XML = ET.SubElement(WEBXML, "xml", {"xmlns":"https://developers.google.com/blockly/xml"})
@@ -99,7 +93,6 @@ def UnLock():
 def Lock():
     _block("Lock")
 
-
 def Delay(time = 1000):
     b = _block("Delay")
     _field(b, "delay", 0)
@@ -122,11 +115,56 @@ def TakeOff(alt=120):
 def Land():
     b = _block("Land")
 
-def MoveToCoord(x, y, z=120):
+def MoveToCoord(x, y, z = 120):
+    global POS
     b = _block("MoveToCoord")
     _field(b, "X", x)
     _field(b, "Y", y)
     _field(b, "Z", z)
+    POS = [x, y, z]
+
+def MoveToCoord_AutoDelay(x, y, z = 120, v = 100, a = 100, time = 0):
+    global POS
+    b = _block("MoveToCoord")
+    _field(b, "X", x)
+    _field(b, "Y", y)
+    _field(b, "Z", z)
+
+    d = math.sqrt((x - POS[0])**2 + (y - POS[1])**2 + (z - POS[2])**2)
+    t = v / a
+    D = (v**2) / (2 * a)
+    if d > 2 * D:
+        T = 2 * t + (d - 2 * D) / v
+    else:
+        T = 2 * math.sqrt(d / a)
+    
+    T = round(T * 1000)
+    d = round(d)
+
+    Delay(T + time)
+    POS = [x, y, z]
+
+    return d
+
+def Move_Circle(x, y, z = 120, n = 8, r = 100, d = 1400):
+    angle = 2 * math.pi / n
+    for i in range(n):
+        theta = i * angle
+        dx = round(r * math.cos(theta))
+        dy = round(r * math.sin(theta))
+        MoveToCoord(x + dx, y + dy, z)
+        Delay(d)
+    MoveToCoord(x + round(r * math.cos(0)), y + round(r * math.sin(0)), z)
+    Delay(d)
+
+def Move_Circle_AutoDeplay(x, y, z = 120, n = 8, r = 100, v = 100, a = 100, time = 0):
+    angle = 2 * math.pi / n
+    for i in range(n):
+        theta = i * angle
+        dx = round(r * math.cos(theta))
+        dy = round(r * math.sin(theta))
+        MoveToCoord_AutoDelay(x + dx, y + dy, z, v, a, time)
+    MoveToCoord_AutoDelay(x + round(r * math.cos(0)), y + round(r * math.sin(0)), z, v, a, time)
 
 def LedAllOn(color="#ffffff"):
     b = _block("LedAllOn")
@@ -158,9 +196,14 @@ STATEMENT = None
 FATHER = None
 WEBXML = None
 XML = None
+POS = [0, 0, 0]
 DRONE_NUM = 0
+
 if __name__ == "__main__":
-    start()
+    DRONE = [
+        [160, 160],
+    ]
+    start(DRONE)
     StartTime()
     UnLock()
     Delay()
@@ -177,28 +220,3 @@ if __name__ == "__main__":
     LedAllOn()
     Lock()
     finish()
-
-    start()
-    StartTime()
-    UnLock()
-    Delay()
-    Horizontal()
-    Vertical()
-    TakeOff()
-    MoveToCoord(260, 160)
-    Delay(2000)
-    Land()
-    Delay(2500)
-    Lock()
-    finish()
-
-    start()
-    finish()
-
-    start()
-    finish()
-
-    start()
-    finish()
-
-    save()
